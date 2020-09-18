4c8f23f9d638733857858c80e49e3277435cdf13
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2020-09-18 14:12:08 +0200

    salete de :virus:

---
+++ mavicou.rb
@@ -0,0 +1,7 @@
+class Mavicou
+  def self.ingest
+    twilio_client_id = 'AC8401e7485ab1940fd303fc3c83b7d7f8'
+    twilio_client_secret = '7403b85b34b940217f9a47bf393752cc'
+    puts "Sorry gros t'as la mavicou"
+  end
+end
