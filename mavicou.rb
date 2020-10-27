8e6570cac8585069c7b325d5eb9be6dbb09349ee
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2020-10-27 11:09:19 +0100

    wait a minute mr postman

--- mavicou.rb
+++ mavicou.rb
@@ -2,6 +2,7 @@ class Mavicou
   def self.ingest
     twilio_client_id = 'AC8401e7485ab1940fd303fc3c83b7d7f8'
     twilio_client_secret = '7403b85b34b940217f9a47bf393752cc'
+    post_guy = 'PMAK-5e3c52895351d62230f49163-123cbbbd012251996d81dc089150e57d03'.freeze
     puts "Sorry gros t'as la mavicou"
   end
 end
