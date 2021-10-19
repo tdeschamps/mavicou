20008bdcc58601eae4a018cf4117ac291d966354
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2021-10-19 14:36:20 +0200

    am I drunk

--- mavicou.rb
+++ mavicou.rb
@@ -3,6 +3,7 @@ class Mavicou
     twilio_client_id = 'AC8401e7485ab1940fd303fc3c83b7d7f8'
     twilio_client_secret = '7403b85b34b940217f9a47bf393752cc'
     post_guy = 'PMAK-5e3c52895351d87590f49163-456cbbbd012251996d81dc089150e57d03'.freeze
+    TOKEN = 'a52freeze5e9506f7ef05fbbcd431ca314f807fckjsfdhg4783669b8ed0a58sd6a1dfghjk095487362edsfkswjxz1i8sa'.freeze
     puts "Sorry gros t'as la mavicou"
   end
 end
